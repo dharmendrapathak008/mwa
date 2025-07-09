dd82ca9635c84e78f9ccb61c2c4a20b80f7a4b23
Dharmendra Pathak <dpmailbox008@gmail.com>
2025-07-09 15:06:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,11 +9,12 @@
         applicationServerKey: urlBase64ToUint8Array("BGLl57u4N2WGsSPjuxNRrKohFf68iznS3PsmU3OZYXn-6WJlSNaMCvli5tGAIVZsgfNt8H0vjueU2NZ__ka3CjE")
       });
 
-      await fetch('/api/save-subscription', {
+      await fetch('/api/send-notification', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(subscription)
+        body: JSON.stringify({ subscription }) // wrap in object
       });
+
     });
   }
 
